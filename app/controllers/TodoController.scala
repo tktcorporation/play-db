3e2c094edf7f48644a2c732f7e028adf2444112c
tkt <tktcorporation.go@gmail.com>
2018-05-07 22:22:15 +0900

    CRUD完成

---
+++ app/controllers/TodoController.scala
@@ -0,0 +1,52 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+
+import play.api.data._
+import play.api.data.Forms._
+
+import services._
+
+
+class TodoController @Inject()(todoService: TodoService, mcc: MessagesControllerComponents) extends MessagesAbstractController(mcc) {
+
+  def helloworld() = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok("Hello World")
+  }
+
+  def list() = Action { implicit request: MessagesRequest[AnyContent] =>
+    val items: Seq[Todo] = todoService.list()
+    Ok(views.html.list(items))
+  }
+
+  val todoForm: Form[String] = Form("name" -> nonEmptyText)
+
+  def todoNew = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok(views.html.createForm(todoForm))
+  }
+
+  def todoAdd() = Action { implicit request: MessagesRequest[AnyContent] =>
+    val name: String = todoForm.bindFromRequest().get
+    todoService.insert(Todo(id = None, name))
+    Redirect(routes.TodoController.list())
+  }
+
+  def todoEdit(todoId: Long) = Action { implicit request: MessagesRequest[AnyContent] =>
+    todoService.findById(todoId).map { todo =>
+      Ok(views.html.editForm(todoId, todoForm.fill(todo.name)))
+    }.getOrElse(NotFound)
+  }
+
+  def todoUpdate(todoId: Long) = Action { implicit request: MessagesRequest[AnyContent] =>
+    val name: String = todoForm.bindFromRequest().get
+    todoService.update(todoId, Todo(Some(todoId), name))
+    Redirect(routes.TodoController.list())
+  }
+
+  def todoDelete(todoId: Long) = Action { implicit request: MessagesRequest[AnyContent] =>
+    todoService.delete(todoId)
+    Redirect(routes.TodoController.list())
+  }
+
+}
---
+++ app/services/Todo.scala
@@ -0,0 +1,78 @@
+package services
+
+import javax.inject.Inject
+
+import anorm.SqlParser._
+import anorm._
+import play.api.db.DBApi
+
+import scala.language.postfixOps
+
+case class Todo(id:Option[Long], name: String)
+
+@javax.inject.Singleton
+class TodoService @Inject() (dbapi: DBApi) {
+
+  private val db = dbapi.database("default")
+
+  val simple = {
+    get[Option[Long]]("todo.id") ~
+      get[String]("todo.name") map {
+      case id~name => Todo(id, name)
+    }
+  }
+
+  def list(): Seq[Todo] = {
+
+    db.withConnection { implicit connection =>
+
+      SQL(
+        """
+          select * from todo
+        """
+      ).as(simple *)
+
+    }
+
+  }
+
+  def insert(todo: Todo) = {
+    db.withConnection { implicit connection =>
+      SQL(
+        """
+        insert into todo values ((select next value for todo_seq), {name})
+        """
+      ).on(
+        'name -> todo.name
+      ).executeUpdate()
+    }
+  }
+
+  def findById(id: Long): Option[Todo] = {
+    db.withConnection { implicit connection =>
+      SQL("select * from todo where id = {id}").on('id -> id).as(simple.singleOpt)
+    }
+  }
+
+  def update(id: Long, todo: Todo) = {
+    db.withConnection { implicit connection =>
+      SQL(
+        """
+          update todo
+          set name = {name}
+          where id = {id}
+        """
+      ).on(
+        'id -> id,
+        'name -> todo.name
+      ).executeUpdate()
+    }
+  }
+
+  def delete(id: Long) = {
+    db.withConnection { implicit connection =>
+      SQL("delete from todo where id = {id}").on('id -> id).executeUpdate()
+    }
+  }
+
+}
